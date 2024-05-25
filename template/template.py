4f81dc775a3906504a5a734bf69aaca1139e0a0d
TravHaran <ratnahat@mcmaster.ca>
2024-05-25 12:16:46 -0400

    add template python class

---
+++ template/template.py
@@ -0,0 +1,32 @@
+'''
+Create a class to model a character in a video game
+- initialize the class with three parameters
+    - Health
+    - Damage
+    - Speed
+
+- define a mathod to double the speed of the character
+'''
+
+class Character:
+        def __init__(self, health, damage, speed):
+            self.health = health
+            self.damage = damage
+            self.speed = speed
+        
+        def double_speed(self):
+                self.speed *= 2
+
+
+
+
+warrior = Character(100, 50, 10)
+ninja = Character(80, 40, 40)
+
+print(f"Warrior speed: {warrior.speed}")
+print(f"Ninja speed: {ninja.speed}")
+
+warrior.double_speed()
+
+print(f"Warrior speed: {warrior.speed}")
+  
