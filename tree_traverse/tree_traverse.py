b88bae24747f6c5e020e5950e7372044a2762ac7
TravHaran <ratnahat@mcmaster.ca>
2024-05-27 11:39:15 -0400

    modified tree_traverse to return the results object

--- tree_traverse/tree_traverse.py
+++ tree_traverse/tree_traverse.py
@@ -1,20 +1,21 @@
-import json
+import sys
+
+sys.path.insert(0, "..")
+from utilities.utility import obj_to_json, json_to_obj
 
 '''
 Create a class to find the most relevant node in the codebase model given some keywords
 - input:
     - list of keywords
-    - codebase model json file
+    - codebase model object
 - output:
-    - dictionary object of top nodes containing: file_name, annotation, content, and matching_keywords
-    - optionally save result as json
+    - object of top nodes containing: file_name, annotation, content, and matching_keywords
 '''
 
 
 class TraverseCodebase:
-    def __init__(self, model_file_path):
-        self.file_path = model_file_path
-        self.model = self.load_model()
+    def __init__(self, model_obj):
+        self.model = model_obj
         self.top_nodes_with_score = []
         self.result_file_name = "result"
 
@@ -38,10 +39,9 @@ class TraverseCodebase:
         common_elems = input_set.intersection(target_set)
         return list(common_elems)
 
-    def get_top_nodes(self, input_keywords, n, result_file_name):
+    def get_top_nodes(self, input_keywords, n):
         # we need to reset the top_nodes list to empty so that multiple calls of this method don't append to it
         self.top_nodes_with_score = []
-        self.result_file_name = result_file_name
         # lower input keywords to compute score properly
         input_keywords_lowered = self.convert_keywords_to_lowercase(
             input_keywords)
@@ -49,8 +49,8 @@ class TraverseCodebase:
         self._get_top_nodes(self.model, input_keywords_lowered)
         # after traversal sort top_nodes list by score in descending order
         self.top_nodes_with_score.sort(key=lambda x: x[0], reverse=True)  
-        # save result
-        return self.save_top_n(n, input_keywords_lowered)
+        # return result
+        return self.build_result(n, input_keywords_lowered)
 
     def _get_top_nodes(self, model, input_keywords):
         if model["type"] == "file":
@@ -62,8 +62,7 @@ class TraverseCodebase:
             for child in model["children"]:
                 self._get_top_nodes(child, input_keywords)
 
-    def save_top_n(self, n, input_keywords):
-        # save the top n nodes as json
+    def build_result(self, n, input_keywords):
         result = {"input_keywords": input_keywords, "results": []}
         for entry in self.top_nodes_with_score:
             score = entry[0]
@@ -74,20 +73,7 @@ class TraverseCodebase:
             entry = {'score': score, 'matched_keywords': matched_keywords, 'node': node}
             result["results"].append(entry)
         result["results"] = result["results"][:n]
-        self.save_json(result)
         return result
-
-    def load_model(self):
-        d = {}
-        with open(self.file_path) as json_data:
-            d = json.load(json_data)
-        return d
-
-    def save_json(self, json_obj):
-        save_file = open(f"{self.result_file_name}.json", 'w')
-        json.dump(json_obj, save_file, indent=4)
-        save_file.close()
-        print(f"Result saved: {self.result_file_name}.json")
 
     def convert_keywords_to_lowercase(self, keywords):
         return [word.lower() for word in keywords]
@@ -95,38 +81,32 @@ class TraverseCodebase:
 
 class TestTraverseCodebase:
     def __init__(self):
-        self.test_json_file = "/Users/trav/Documents/projects/codesense/tree_traverse/test_codebase.json"
-        self.traverser = TraverseCodebase(self.test_json_file)
+        self.test_model = json_to_obj("test_codebase.json")
+        self.traverser = TraverseCodebase(self.test_model)
 
     def test_save_top_1_nodes(self):
         print(f"Testing Traverse Codebase to save top 1 nodes")
         input_keywords = ["Python", "function", "TestKeywordExtract",
                           "NLTK", "Word2Vec", "extract_keywords"]
-        self.traverser.get_top_nodes(input_keywords, 1, "top_1")
+        updated_model = self.traverser.get_top_nodes(input_keywords, 1)
+        obj_to_json("./", "top_1", updated_model)
+        assert type(updated_model) == dict
 
     def test_save_top_3_nodes(self):
         print(f"Testing Traverse Codebase to save top 3 nodes")
         input_keywords = ["Python", "function", "TestKeywordExtract",
                           "NLTK", "Word2Vec", "extract_keywords"]
-        self.traverser.get_top_nodes(input_keywords, 3, "top_3")
+        updated_model = self.traverser.get_top_nodes(input_keywords, 3)
+        obj_to_json("./", "top_3", updated_model)
+        assert type(updated_model) == dict
 
     def test_save_top_5_nodes(self):
         print(f"Testing Traverse Codebase to save top 5 nodes")
         input_keywords = ["Python", "function", "TestKeywordExtract",
                           "NLTK", "Word2Vec", "extract_keywords"]
-        self.traverser.get_top_nodes(input_keywords, 5, "top_5")
-
-    def test_save_top_10_nodes(self):
-        print(f"Testing Traverse Codebase to save top 10 nodes")
-        input_keywords = ["Python", "function", "TestKeywordExtract",
-                          "NLTK", "Word2Vec", "extract_keywords"]
-        self.traverser.get_top_nodes(input_keywords, 10, "top_10")
-    
-    def test_save_top_50_nodes(self):
-        print(f"Testing Traverse Codebase to save top 50 nodes")
-        input_keywords = ["Python", "function", "TestKeywordExtract",
-                          "NLTK", "Word2Vec", "extract_keywords"]
-        self.traverser.get_top_nodes(input_keywords, 50, "top_50")
+        updated_model = self.traverser.get_top_nodes(input_keywords, 5)
+        obj_to_json("./", "top_5", updated_model)
+        assert type(updated_model) == dict
 
 
 if __name__ == "__main__":
@@ -134,5 +114,4 @@ if __name__ == "__main__":
     testTraverseCodebase.test_save_top_1_nodes()
     testTraverseCodebase.test_save_top_3_nodes()
     testTraverseCodebase.test_save_top_5_nodes()
-    testTraverseCodebase.test_save_top_10_nodes()
-    testTraverseCodebase.test_save_top_50_nodes()
+
